e2a38316bf8a6be4732695090e8d7484c40b4b6a
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-09-13 11:56:11 +0300

    Ow ye

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,38 +1,39 @@
-const { MessageButton, Collector } = require("discord.js");
+const { MessageButton, Collector,MessageActionRow } = require("discord.js");
 
 module.exports = {
     name:"disconnect",
     async run(client, message, args){
         if(message.author.id !== "952940471368450068") return;
-        const btna = new MessageButton()
-        .setStyle("DANGER")
-        .setLabel("Accept")
-        .setCustomId("acp")
-        const btnd = new MessageButton()
-        .setStyle("SUCCESS")
-        .setLabel("Decline")
-        .setCustomId("decline");
+        const btnaccept = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setStyle("DANGER")
+            .setLabel("Accept")
+            .setCustomId("acp")
+        )
+        const btndecline = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setStyle("SUCCESS")
+            .setLabel("Decline")
+            .setCustomId("decline")
+        )
 
-        const filter1 = i => i.customId === "acp" || i.user.id === message.author.id;
 
-        const collector = message.channel.createMessageCollector({filter1});
-
+        let iFilter = i => i.user.id === "952940471368450068";
+        let collector = message.createMessageComponentCollector({ filter: iFilter, time: 3600000 })
+        
         collector.on("collect", async(i) => {
-            setTimeout(function(){
-                client.destroy()
-                //process.exit(1);
-            }, 3000)
+            if(i.customId === "acp"){
+                i.reply("bb :(").then(setTimeout(function(){
+                    process.exit(0);
+                },3000))
+            
+            } else if (i.customId === "decline"){
+                i.reply("İptal Edildi.")
+            }
         })
-
-        const filter2 = i => i.customId === "decline" 
-
-        const collector2 = message.channel.createMessageCollector({filter2});
-
-        collector2.on("collect", async(i) => {
-            i.reply({content:"Canceled Process"})
-        })
-
-       await message.channel.send({content:"Disconnect", components:[btna,btnd]})
+       await message.channel.send({content:"Disconnect", components:[btnaccept, btndecline]})
 
     }
 }
